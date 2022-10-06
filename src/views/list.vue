ea4375952be60ecc25f4d49490e8d1a7c68f4de3
wangluan <wanglhwww@163.com>
2022-10-06 12:04:24 +0800

    feat:添加一个文件list

---
+++ src/views/list.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>列表页</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  data() {
+    return {};
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>
